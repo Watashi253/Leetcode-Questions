400fee11c48fc714a9bcb53c6319d73aab29acd0
Anandita Gupta <ananditagupta253@gmail.com>
2026-04-13 15:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (35.42%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map <int,int> hash;
+        stack <int> st; 
+        for (int i=nums2.size()-1; i>=0 ; i--) {
+            while ( !st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                hash[nums2[i]] = -1;
+            } 
+            else {
+                hash[nums2[i]] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+vector <int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            ans.push_back(hash[nums1[i]]);
+        }
+        return ans;  
+    }
+};

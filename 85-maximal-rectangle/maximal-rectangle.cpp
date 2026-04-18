c401dd5340be54b4ba8d3db5ce18157d8153d001
Anandita Gupta <ananditagupta253@gmail.com>
2026-04-19 04:04:41 +0530

    Time: 8 ms (55.17%) | Memory: 19.6 MB (55.03%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int lHist(vector<int>& heights) {
+        stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int el = st.top();
+                st.pop();
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+                maxArea = max(heights[el] * (nse - pse - 1), maxArea);
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            int nse = n;
+            int el = st.top();
+            st.pop();
+            int pse = st.empty() ? -1 : st.top();
+            maxArea = max(maxArea, (nse - pse - 1) * heights[el]);
+        }
+        return maxArea;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int maxArea = 0;
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> psum(n, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    psum[j] += 1;
+                else
+                    psum[j] = 0;
+            }
+            maxArea = max(maxArea, lHist(psum));
+        }
+        return maxArea;
+    }
+};

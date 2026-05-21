49d2b63431b39d92bc6ca3f842a947d9eeedde05
Anandita Gupta <ananditagupta253@gmail.com>
2026-05-21 16:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (63.04%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,20 +14,17 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         if(root==NULL) return ans;
-        queue<pair<int, TreeNode*>>q;
-        map<int, int> hash;
-        q.push({0,root});
+        queue<TreeNode*>q;
+        q.push(root);
         while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            TreeNode* node=it.second;
-            int row=it.first;
-            hash[row]=node->val;
-            if(node->left)q.push({row+1, node->left});
-            if(node->right)q.push({row+1, node->right});
-        }
-        for(auto it: hash){
-            ans.push_back(it.second);
+            int n=q.size();
+            for(int i=1; i<=n; i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(i==n) ans.push_back(node->val);
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
+            }
         }
         return ans;
     }

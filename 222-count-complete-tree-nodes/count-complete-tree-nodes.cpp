7a256c7053bbdef0adac3bec89d91c736f479817
Anandita Gupta <ananditagupta253@gmail.com>
2026-05-27 17:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (13.09%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -6,13 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    int countLeftlevel(TreeNode* node) {
+        int h = 0;
+        while (node) {
+            h++;
+            node = node->left;
+        }
+        return h;
+    }
+    int countRightlevel(TreeNode* node) {
+        int h = 0;
+        while (node) {
+            h++;
+            node = node->right;
+        }
+        return h;
+    }
     int countNodes(TreeNode* root) {
-        if(root==NULL) return 0;
-        return countNodes(root->left)+countNodes(root->right)+1;
+        if (root == NULL)
+            return 0;
+
+        int lh = countLeftlevel(root);
+        int rh = countRightlevel(root);
+
+        if (rh == lh)
+            return pow(2, lh) - 1;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };

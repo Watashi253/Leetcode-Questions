42947b122d3213614f0bffa14c2457322030f6a3
Anandita Gupta <ananditagupta253@gmail.com>
2026-05-30 13:07:35 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (13.86%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -19,12 +19,14 @@ void find(TreeNode* root, vector<int> & inorder){
 }
     bool findTarget(TreeNode* root, int k) {
         vector<int> inorder;
+        set<int> look;
         find(root, inorder);
-        for(int i=0; i<inorder.size()-1; i++){
+        // look.push_back(k-inorder[0]);
+        for(int i=0; i<inorder.size(); i++){
             int sum=k-inorder[i];
-            for(int j=i+1; j<inorder.size(); j++){
-                if(sum==inorder[j]) return true;
-            }
+            if(look.find(sum)!=look.end())
+            return true;
+            look.insert(inorder[i]);
         }
         return false;
     }

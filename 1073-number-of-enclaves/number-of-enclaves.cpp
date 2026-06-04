e291af5d1b9ff4c5b7268c7013ddd4fbecff0287
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-04 10:44:01 +0530

    Time: 7 ms (74.98%) | Memory: 31.7 MB (69.85%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int num=0;
-        int walk=0;
-        vector<vector<int>> vis(n, vector<int>(m,0));
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n,0));
         queue<pair<int, int>> q;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==1){
-                    num++;
-                    if(i==0 || j==0 || i==n-1 || j==m-1){
-                        vis[i][j]=1;
-                        q.push({i,j});
-                        walk++;
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            if(grid[0][i] == 1) {
+                vis[0][i]=1;
+                q.push({0,i});
+            }
+            if(grid[m - 1][i] == 1) {
+                vis[m-1][i]=1;
+                q.push({m-1,i});
+            }
+        }
+
+        for (int j = 0; j < m; j++) {
+            if(grid[j][0] == 1) {
+                vis[j][0]=1;
+                q.push({j,0});
+            }
+            if(grid[j][n - 1] == 1) {
+                vis[j][n-1]=1;
+                q.push({j,n-1});
             }
         }
 
@@ -32,15 +39,23 @@ public:
                 int nrow=row+dr[i];
                 int ncol=col+dc[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n &&
                 !vis[nrow][ncol] && grid[nrow][ncol]==1){
                     vis[nrow][ncol]=1;
                     q.push({nrow,ncol});
-                    walk++;
                 }
             }
         }
 
-        return num-walk;
+        int ans=0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    vis[i][j]=1;
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 };

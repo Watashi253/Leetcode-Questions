42dd67309ff9f27b752921e28b41b22da89d8352
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-23 14:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (75.88%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) { 
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-
-        dp[0][0]=1;
+        vector<int> prev(n,0);
         for(int i=0; i<m; i++){
+            vector<int> temp(n,1);
             for(int j=0; j<n; j++){
-                if(i==0 && j==0) continue;
-                dp[i][j]= 0;
-                if(i-1 >=0)dp[i][j]+=dp[i-1][j];
-                if(j-1 >=0)dp[i][j]+=dp[i][j-1];
+                if(j-1>=0)temp[j] = prev[j]+temp[j-1];
             }
+            prev=temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
         }
 };

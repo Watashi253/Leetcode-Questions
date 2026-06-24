3e085447c454ebbb1d95a7afecaaacd83953d311
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-24 12:29:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (8.48%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,23 +3,22 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
          int n=triangle.size();
          int ans=INT_MAX;
-         vector<vector<int>> dp = triangle;;
-         
-         dp[0][0]=triangle[0][0];
-
-         for(int i=1; i<n; i++){
-            for(int j=0; j<triangle[i].size(); j++){
-                int up=1e9, upl=1e9;
-                if(i-1>=0 && j<triangle[i-1].size())
-                up= triangle[i][j]+dp[i-1][j];
-                if(i-1>=0 && j-1>=0)
-                upl= triangle[i][j]+dp[i-1][j-1];
-                dp[i][j]=min(up, upl);
+         vector<vector<int>> dp = triangle;
+        
+         for(int i=n-1; i>=0; i--){
+            for(int j=triangle[i].size()-1; j>=0; j--){
+                if(i==n-1){
+                    dp[i][j]=triangle[i][j];
+                }
+                else{
+                int d=1e9, dr=1e9;
+                if(i+1<n) d= triangle[i][j]+dp[i+1][j];
+                if(i+1<n) dr= triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(d, dr);
+                }
             }
          }
-         for(int i=0; i<triangle[n-1].size(); i++){
-           ans=min(ans,dp[n-1][i]);
-         }
-         return ans;
+
+         return dp[0][0];
     }
 };

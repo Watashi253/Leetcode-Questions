92dc70ab63899c99f4c27c77fd93b9db03c6ac21
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-24 12:57:44 +0530

    Time: 3 ms (37.09%) | Memory: 15.1 MB (43.14%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -5,24 +5,24 @@ vector<vector<int>> &dp){
 if(col<0 || col>=matrix.size())
 return 1e9;
 
-if(row==matrix.size()-1){
+if(row==0){
     return dp[row][col]= matrix[row][col];
 }
 
 if(dp[row][col]!=1e9) return dp[row][col];
 
-int d=matrix[row][col]+ check(row+1, col, matrix, dp);
-int dl=matrix[row][col]+ check(row+1, col-1, matrix, dp);
-int dr=matrix[row][col]+ check(row+1, col+1, matrix, dp);
+int u=matrix[row][col]+ check(row-1, col, matrix, dp);
+int ur=matrix[row][col]+ check(row-1, col+1, matrix, dp);
+int ul=matrix[row][col]+ check(row-1, col-1, matrix, dp);
 
-return dp[row][col]= min(d, min(dl, dr));
+return dp[row][col]= min(u, min(ul, ur));
 }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         vector<vector<int>>dp(n, vector<int>(n,1e9)); 
         int ans=1e9;
         for(int i=0; i<n; i++){
-            ans=min(ans, check(0, i, matrix, dp));
+            ans=min(ans, check(n-1, i, matrix, dp));
         }
        return ans;
     }

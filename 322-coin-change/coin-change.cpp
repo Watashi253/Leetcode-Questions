43d85faddc7ac2599fff0fca27b42b4d4ceb9133
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-27 19:57:05 +0530

    Time: 49 ms (26.65%) | Memory: 49 MB (35.02%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-int check(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp){
-if(ind==0){
-    if(amount % coins[0] == 0) return amount / coins[0];
-    return 1e9;
-}
-if(dp[ind][amount]!=-1) return dp[ind][amount];
-
-int notake=check(ind-1, amount, coins, dp);
-int take=1e9;
-if(coins[ind]<=amount){
-    take=1+check(ind, amount - coins[ind], coins, dp);
-}
-return dp[ind][amount]=min(take, notake);
-}
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        int ans=check(n-1, amount, coins, dp);
-        return ans>=1e9?-1:ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        for(int i=0; i<=amount; i++){
+            if (i % coins[0] == 0)
+            dp[0][i]= i / coins[0];
+            else dp[0][i]=1e8;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int j = 0; j <= amount; j++) {
+                int notake = dp[ind - 1][j];
+                int take = 1e8;
+                if (coins[ind] <= j) {
+                    take = 1 + dp[ind][j - coins[ind]];
+                }
+                dp[ind][j] = min(take, notake);
+            }
+        }
+
+        return dp[n-1][amount] >= 1e8 ? -1 : dp[n-1][amount];
     }
 };

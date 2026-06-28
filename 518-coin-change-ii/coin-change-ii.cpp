4cbf0f59f5cd4e5589d2084cdd9918b5fd7ba564
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-28 11:46:18 +0530

    Time: 19 ms (74.83%) | Memory: 10.3 MB (91.09%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-int check(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp){
-    if(amt==0) return 1;
-    if(ind==0){
-        if(amt%coins[0]==0) return 1;
-        return 0;
-    }
+    int check(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp) {
+        if (amt == 0)
+            return 1;
+        if (ind == 0) {
+            if (amt % coins[0] == 0)
+                return 1;
+            return 0;
+        }
 
-    if(dp[ind][amt]!=-1) return dp[ind][amt];
+        if (dp[ind][amt] != -1)
+            return dp[ind][amt];
 
-    int notake=check(ind-1, amt, coins, dp);
-    int take=0;
-    if(coins[ind]<=amt){
-        take=check(ind, amt-coins[ind], coins, dp);
+        int notake = check(ind - 1, amt, coins, dp);
+        int take = 0;
+        if (coins[ind] <= amt) {
+            take = check(ind, amt - coins[ind], coins, dp);
+        }
+        return dp[ind][amt] = take + notake;
     }
-    return dp[ind][amt]=take+notake;
-}
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return check(n-1, amount, coins, dp);
+        int n = coins.size();
+        vector<int> prev(amount + 1, 0), cur(amount + 1, 0);
+        prev[0]=cur[0]=1;
+        for(int i=1; i<=amount; i++){
+            if (i % coins[0] == 0) prev[i]=1;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int amt = 1; amt <= amount; amt++) {
+                long long notake = prev[amt];
+                long long take = 0;
+                if (coins[ind] <= amt) {
+                    take = cur[amt - coins[ind]];
+                }
+                cur[amt] = take + notake;
+            }
+            prev=cur;
+        }
+        return prev[amount];
     }
 };

cba8f90070cecc391287a76538a358c148b6422b
Anandita Gupta <ananditagupta253@gmail.com>
2026-06-28 11:56:53 +0530

    Time: 6 ms (88.38%) | Memory: 10 MB (99.25%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,42 +1,14 @@
 class Solution {
 public:
-    int check(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp) {
-        if (amt == 0)
-            return 1;
-        if (ind == 0) {
-            if (amt % coins[0] == 0)
-                return 1;
-            return 0;
-        }
-
-        if (dp[ind][amt] != -1)
-            return dp[ind][amt];
-
-        int notake = check(ind - 1, amt, coins, dp);
-        int take = 0;
-        if (coins[ind] <= amt) {
-            take = check(ind, amt - coins[ind], coins, dp);
-        }
-        return dp[ind][amt] = take + notake;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<int> prev(amount + 1, 0), cur(amount + 1, 0);
-        prev[0]=cur[0]=1;
-        for(int i=1; i<=amount; i++){
-            if (i % coins[0] == 0) prev[i]=1;
-        }
+        vector<unsigned int> prev(amount + 1, 0);
+        prev[0]=1;
 
-        for (int ind = 1; ind < n; ind++) {
-            for (int amt = 1; amt <= amount; amt++) {
-                long long notake = prev[amt];
-                long long take = 0;
-                if (coins[ind] <= amt) {
-                    take = cur[amt - coins[ind]];
-                }
-                cur[amt] = take + notake;
+        for (int coin: coins) {
+            for (int amt = coin; amt<=amount; amt++) {
+                prev[amt] += prev[amt-coin];
             }
-            prev=cur;
         }
         return prev[amount];
     }

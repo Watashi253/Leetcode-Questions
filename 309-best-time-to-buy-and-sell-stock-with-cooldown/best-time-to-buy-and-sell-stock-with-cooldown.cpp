0ef25fb445716a099e59fd9cc4a0ab5cbe72d66f
Anandita Gupta <ananditagupta253@gmail.com>
2026-07-04 17:50:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (79.67%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,14 +18,14 @@ int f(int i, int buy, vector<int>& prices, vector<vector<int>>& dp){
 }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<int> next(3,0), cur(3,0);
+        vector<int> cur(3,0);
 
         for(int i=n-1; i>=0; i--){
-            cur[0]=max(-prices[i]+next[1], next[0]);
-            cur[1]=max(+prices[i]+next[2], next[1]);
-            cur[2]=next[0];
-            next=cur;
+            int hold=cur[0];
+            cur[0]=max(-prices[i]+cur[1], cur[0]);
+            cur[1]=max(+prices[i]+cur[2], cur[1]);
+            cur[2]=hold;
         }
-        return next[0];
+        return cur[0];
     }
 };

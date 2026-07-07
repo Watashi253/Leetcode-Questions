569de5f188dc49f9787b1ef99107ca7faa60a85c
Anandita Gupta <ananditagupta253@gmail.com>
2026-07-07 11:16:03 +0530

    Time: 37 ms (80.70%) | Memory: 22.1 MB (47.62%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -3,34 +3,22 @@ public:
 static bool comp(string &s1, string &s2){
     return s1.size()<s2.size();
 }
-bool check(string &s1, string &s2){
-    if(s1.size()-s2.size()!=1) return false;
-    int i=0, j=0;
-    while(i<s1.size()){
-        if(j < s2.size() && s1[i]==s2[j]){
-            i++, j++;
-        }
-        else i++;
-    }
-    if(i==s1.size() && j==s2.size()) return true;
-    return false;
-}
     int longestStrChain(vector<string>& words) {
         int n=words.size();
         sort(words.begin(), words.end(), comp);
         
-        vector<int> dp(n, 1);
+        unordered_map<string, int> dp;
         int maxi=1;
 
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(check(words[i], words[j])){
-                    dp[i] = max(dp[i], 1 + dp[j]);
+        for(auto word: words){
+            dp[word]=1;
+            for(int j=0; j<word.size(); j++){
+                string pred=word.substr(0,j)+word.substr(j+1);
+                if(dp.find(pred) != dp.end()){
+                    dp[word] = max(dp[word], 1 + dp[pred]);
                 }
             }
-            if(dp[i]>maxi){
-                maxi=dp[i];
-            }
+            maxi=max(dp[word], maxi);
         }
         return maxi;
     }

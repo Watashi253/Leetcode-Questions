5fd2d5a96b61c7abed03715181497a9fb1e436a6
Anandita Gupta <ananditagupta253@gmail.com>
2026-08-15 20:48:21 +0530

    Time: 1514 ms (7.70%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -5,9 +5,5 @@ FROM Students st
 CROSS JOIN Subjects s
 LEFT JOIN Examinations e
 ON e.student_id=st.student_id AND e.subject_name=s.subject_name
-GROUP BY st.student_id, 
-    st.student_name, 
-    s.subject_name
-ORDER BY 
-    st.student_id, 
-    s.subject_name;
+GROUP BY st.student_id, st.student_name, s.subject_name
+ORDER BY st.student_id, s.subject_name;

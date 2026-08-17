a618ca500f566138b1cddf91c9cfc756803ba18c
Anandita Gupta <ananditagupta253@gmail.com>
2026-08-17 16:31:39 +0530

    Time: 423 ms (83.64%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
 SELECT 
 MAX(num) AS num
-FROM MyNumbers
-WHERE(num) IN (
+FROM (
     SELECT num
     FROM MyNumbers
     GROUP BY num
     HAVING COUNT(num)=1
-);
+) AS single_num;

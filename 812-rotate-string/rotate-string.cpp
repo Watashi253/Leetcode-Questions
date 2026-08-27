4c074fe40f15e7b3eee02c2857b12b5dc8c8317f
Anandita Gupta <ananditagupta253@gmail.com>
2026-08-27 09:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (55.95%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.size();
+
+        for(int i=0; i<n; i++){
+            string temp=s;
+            reverse(temp.begin()+i, temp.end());
+            reverse(temp.begin(), temp.begin()+i);
+            reverse(temp.begin(), temp.end());
+
+            if(temp==goal) return true;
+        }
+
+        return false;
+    }
+};

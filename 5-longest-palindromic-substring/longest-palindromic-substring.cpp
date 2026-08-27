15b3ee3fa06a593b5ca32eccc596d4f166301a81
Anandita Gupta <ananditagupta253@gmail.com>
2026-08-27 14:46:13 +0530

    Time: 74 ms (37.47%) | Memory: 164.9 MB (13.41%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+int palindrome(string s, int l, int r){
+  while (l >= 0 && r < s.length() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+return r-l-1;
+}
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+
+        int start = 0;
+        int maxLen = 0;
+
+        for(int i=0; i<s.size(); i++){
+            int len1=palindrome(s, i, i);
+            int len2=palindrome(s, i, i+1);
+
+            int len=max(len1, len2);
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2; 
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
